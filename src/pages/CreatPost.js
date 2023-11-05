e52e666edba6863ca564532644b2e53b5d18862d
lillian0624 <nopeyangli@163.com>
2023-11-05 22:25:01 +1100

    Creat Post

--- src/pages/CreatPost.js
+++ src/pages/CreatPost.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
 
 function CreatPost() {
-  return <div>POST</div>;
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
+  const postCollectionRef = collection(db, "post");
+  let navigate = useNavigate();
+
+  const createPost = async () => {
+    await addDoc(postCollectionRef, {
+      title,
+      postText,
+      author: {
+        name: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      },
+    });
+    navigate("/");
+  };
+
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <lable>Title:</lable>
+        <input
+          placeholder="Title..."
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <div className="inputGp">
+          <label>Post:</label>
+          <textarea
+            placeholder="Post..."
+            onChange={(event) => {
+              setPostText(event.target.value);
+            }}
+          />
+        </div>
+        <button onClick={createPost}>Summit Post</button>
+      </div>
+    </div>
+  );
 }
 
 export default CreatPost;
