89efe00bc05c3b7f0970b65ddeb182ab9866d53d
lillian0624 <nopeyangli@163.com>
2023-11-08 15:37:57 +1100

    init

---
+++ src/components/Auth.js
@@ -0,0 +1,18 @@
+import { auth, provider } from "../firebase-config.js";
+import { signInWithPopup } from "firebase/auth";
+
+import Cookies from "universal-cookie"
+const cookies = new Cookies()
+
+export const Auth = () => {
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    cookies.set("auth-token")
+   
+  return (
+    <div className="auth">
+      <p>Sign In With Google to Continue</p>
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    </div>
+  );
+};
---
+++ src/pages/LiveChat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import { Auth } from "../components/Auth";
+
+function LiveChat() {
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
+}
+
+export default LiveChat;
